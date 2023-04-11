ae264cb46b717eae5c0c900146e9d68c0c769c1a
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-04-12 00:15:46 +0800

    Updates

---
+++ precache-manifest.05e1d4fd51bcc5578412b1b3e1cb455a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0775e8a28d7a5bdfd25",
+    "url": "/PokeDex-React-App/static/css/main.73ea5216.chunk.css"
+  },
+  {
+    "revision": "c0775e8a28d7a5bdfd25",
+    "url": "/PokeDex-React-App/static/js/main.c0775e8a.chunk.js"
+  },
+  {
+    "revision": "c7ac6a3a2c5595bba18e",
+    "url": "/PokeDex-React-App/static/js/1.c7ac6a3a.chunk.js"
+  },
+  {
+    "revision": "ecd816fdc05a21fb7918",
+    "url": "/PokeDex-React-App/static/js/runtime~main.ecd816fd.js"
+  },
+  {
+    "revision": "bc029440ecc96818eddfb96a04b5e0bf",
+    "url": "/PokeDex-React-App/static/media/Pokeball.bc029440.png"
+  },
+  {
+    "revision": "4e6e280304750a665c95c78293651fa5",
+    "url": "/PokeDex-React-App/static/media/UnderConstructionIcon.4e6e2803.png"
+  },
+  {
+    "revision": "dff9fe2f9a9eb6ca256e52d92f8b2abb",
+    "url": "/PokeDex-React-App/index.html"
+  }
+];
