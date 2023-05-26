574609732f888d3070c9cb119d4e77dbb8bebf58
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-05-27 00:46:39 +0800

    CHANGES: Search pokemon data page UI update.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import SearchHomePage from "./Components/PokemonSearchData/SearchHomePage";
 import MapHomePage from "./Components/PokemonMapSearch/MapHomePage";
 import NavbarDesktop from "./Components/Nav/NavbarDesktop";
 import Navbar from "./Components/Nav/Navbar";
-// import LandingPage from "./Components/LandingPage/LandingPage";
+import LandingPage from "./Components/LandingPage/LandingPage";
 import Footer from "./Components/Footer/Footer";
 import LoadingSpinner from "./Components/UI/LoadingSpinner";
 import axios from "axios";
 import ErrorInput from "./Components/UI/ErrorInput";
 import RGBBorder from "./Components/UI/RGBBorder";
-// import "flowbite";
 import "tw-elements";
 import "./App.css";
 
@@ -27,7 +26,7 @@ const App = () => {
   const [mapSearch, setMapSearch] = useState(false);
   const [error, setError] = useState(false);
   const [onModal, setOnModal] = useState(false);
-  const [countingLoading, setCountingLoading] = useState(true); // <-- this must be false
+  const [countingLoading, setCountingLoading] = useState(false); // <-- this must be false
 
   useEffect(() => {
     const pokemonFun = async () => {
@@ -114,10 +113,10 @@ const App = () => {
 
   return (
     <React.Fragment>
-      {/* <LandingPage
+      <LandingPage
         countingLoad={countingLoadingHandler}
         percLoading={isLoading}
-      /> */}
+      />
 
       {/* Comment this when developing ^^ */}
 
